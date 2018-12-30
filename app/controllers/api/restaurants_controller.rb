216bedfce43c2ba5ce7c46f5961a91b9a91ac5e4
Sergey Panchenko <sergey.panchenko@wework.com>
2018-12-30 11:47:05 +0200

    Added create restaurant

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -3,6 +3,16 @@ class Api::RestaurantsController < ApiController
     json_response(_search)
   end
 
+  def create
+    restaurant= Restaurant.create(restaurant_params)
+
+    unless restaurant
+      json_error(error: 'RESTAURANT_CREATE_FAILED', error_message: 'Unable to save restaurant')
+    end
+
+    json_response(restaurant)
+  end
+
   private
 
   def _search
@@ -14,5 +24,17 @@ class Api::RestaurantsController < ApiController
     restaurants = restaurants.having("AVG(reviews.rating) >= ?", params[:rating]) if params[:rating].present?
 
     restaurants.select('"restaurants".*, AVG(reviews.rating) AS rating')
+  end
+
+  def restaurant_params
+    params.require([
+      :name,
+      :cuisine_id,
+      :max_delivery_time,
+      :accept_10_bis,
+      :address_lat,
+      :address_lng,
+      :address_name
+    ])
   end
 end
